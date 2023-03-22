3bec0603743fa745aeceef8e55b629fe716aee42
kcast <kcastaneda2581@gmail.com>
2023-03-22 16:40:43 -0600

    ya se agrego los dos nombres

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def mostrar_menu():
-    print("1. Generar número aleatorio")
-    print("2. Calcular el promedio de una lista de números")
-    print("3. Verificar si un número es primo")
-    print("4. Ordenar una lista de números de forma ascendente")
-    print("5. Multiplicar dos matrices")
-    print("6. Salir")
+import menu
 
+while(menu):
+
+    if menu.llamadas_varias((menu.mostrar_menu())) == 1:
+        break
 
